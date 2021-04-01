a2075aacba2c55a71dee800f0ee6a57ad8437c70
Diego Souza <dfds@outlook.com>
2021-04-01 10:56:10 -0300

    Experiments - 01042021 - 2

---
+++ experiments/Kotlin/Spring/bank/src/main/kotlin/com/codelab/bank/Account.kt
@@ -0,0 +1,13 @@
+package com.codelab.bank
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity(name = "accounts")
+data class Account (
+    @Id @GeneratedValue
+    val id: Long? = null,
+    val name: String,
+    val document: String,
+    val phone: String)
---
+++ experiments/Kotlin/Spring/bank/src/main/kotlin/com/codelab/bank/AccountController.kt
@@ -0,0 +1,45 @@
+package com.codelab.bank
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/accounts")
+class AccountController (private val repository: AccountRepository) {
+
+    @GetMapping
+    fun list(): List<Account> {
+        return repository.findAll()
+    }
+
+    @PostMapping
+    fun create(@RequestBody account: Account): Account {
+        return repository.save(account)
+    }
+
+    @GetMapping("/{id}")
+    fun find(@PathVariable id: Long): ResponseEntity<Account> {
+        return repository.findById(id).map {
+            ResponseEntity.ok(it)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Long, @RequestBody account: Account): ResponseEntity<Account> {
+        return repository.findById(id).map {
+            val draft = it.copy(name = it.name, document = it.document, phone = it.phone)
+            val instance = repository.save(draft)
+            ResponseEntity.ok(instance)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long): ResponseEntity<Void> {
+        return repository.findById(id).map {
+            repository.delete(it)
+            ResponseEntity<Void>(HttpStatus.OK)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+}
