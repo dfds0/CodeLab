c86f260ee57acf9ae630448cbb4260eb26097c8c
Diego Souza <dfds@outlook.com>
2021-04-02 18:15:02 -0300

    Experiments - 02042021 - 3

---
+++ experiments/Kotlin/Spring/bank/src/main/kotlin/com/codelab/bank/handler/ErrorResponse.kt
@@ -0,0 +1,3 @@
+package com.codelab.bank.handler
+
+data class ErrorResponse (val statusCode: Int, val message: String)
--- experiments/Kotlin/Spring/bank/src/main/kotlin/com/codelab/bank/service/AccountServiceImpl.kt
+++ experiments/Kotlin/Spring/bank/src/main/kotlin/com/codelab/bank/service/AccountServiceImpl.kt
@@ -2,20 +2,22 @@ package com.codelab.bank.service
 
 import com.codelab.bank.model.Account
 import com.codelab.bank.repository.AccountRepository
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.util.Assert
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @Service
-class AccountServiceImpl(private val repository: AccountRepository): AccountService {
+class AccountServiceImpl(private val repository: AccountRepository) : AccountService {
 
     override fun list(): List<Account> {
         return repository.findAll()
     }
 
     override fun create(account: Account): Account {
+        Assert.isTrue(account.name.length > 5,"[name] need contains at less 5 characters")
+        Assert.isTrue(account.document.length == 11, "[document] is out of pattern")
+
         return repository.save(account)
     }
 
@@ -23,7 +25,10 @@ class AccountServiceImpl(private val repository: AccountRepository): AccountServ
         return repository.findById(id)
     }
 
-    override fun update(id: Long,account: Account): Optional<Account> {
+    override fun update(id: Long, account: Account): Optional<Account> {
+        Assert.isTrue(account.name.length > 5,"[name] need contains at less 5 characters")
+        Assert.isTrue(account.document.length == 11, "[document] is out of pattern")
+
         val optional = findById(id)
         if (optional.isPresent)
             optional.map {
