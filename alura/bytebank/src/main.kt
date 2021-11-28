a95381265015c95a9d6e91d248103be1fa9a5e7e
Diego <dfds@outlook.com>
2021-11-28 09:22:12 -0300

    Kotlin - 001

---
+++ alura/bytebank/src/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Witamy w ByteBank!")
+
+    val title = "Souza"
+    val account = 9876543210
+    var balance = 0.0
+
+    println("Klient: $title - #$account")
+    println("Bilans konta: $balance")
+}
