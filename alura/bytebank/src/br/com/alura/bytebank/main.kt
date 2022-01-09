5d336caf41a16186a0ae9944b505bddd105dbf37
Diego <dfds@outlook.com>
2022-01-09 20:40:02 -0300

    Kotlin - 037

--- alura/bytebank/src/br/com/alura/bytebank/main.kt
+++ alura/bytebank/src/br/com/alura/bytebank/main.kt
@@ -12,4 +12,5 @@ fun main() {
     testException()
     testNull()
     testFunctions()
+    testLet()
 }
---
+++ alura/bytebank/src/br/com/alura/bytebank/test/testLet.kt
@@ -0,0 +1,52 @@
+package br.com.alura.bytebank.test
+
+import br.com.alura.bytebank.model.client.Address
+
+fun testLet() {
+    println("\n\n### testLet")
+
+    Address().let {
+        println("OBJECT - LET -- $it")
+    }
+    "String".let {
+        println("STRING - LET -- $it")
+    }
+    0.let {
+        println("INT - LET -- $it")
+    }
+
+    val value = orderA(1) { "return operator" }
+    println(value)
+
+    println("\n")
+
+    val address = Address(-1, "SP", "SP", "BR")
+    println(address)
+
+    val _let = address.let {
+        it.city
+    }.let {
+        println(it)
+    }.let {
+        "end"
+    }.let(::println).let {
+        "_let"
+    }
+    println(_let)
+
+    println("\n")
+
+    functionA(1, 2) {
+        println(it)
+    }
+}
+
+fun orderA(value: Int, operator: () -> String) {
+    "$value -- $operator()"
+}
+
+fun functionA(a: Int, b: Int, result: (Int) -> Unit) {
+    println(" -- before")
+    result(a + b)
+    println(" -- after")
+}
